a08920da78538c8f1df5e8d0dd31854225213b66
Abhishek <abhibhovar@gmail.com>
2024-06-28 22:33:46 +0530

    Initial setup completed for client(py_client) and server (django-server)

---
+++ py_client/basic.py
@@ -0,0 +1,16 @@
+import requests
+from pprint import pprint
+"""
+HTTP Request -> HTML Response
+REST API HTTP Request -> JSON Response
+"""
+
+
+# URL from which we can rach the API
+endpoint = "http://localhost:8000/"
+resp = requests.get(endpoint) # sending the request
+# resp = requests.get(endpoint, json = {"query" : "Hello World"}) # sending request with some data
+# print(f"{type(resp.text)} : \n{resp.text}") # prints the resp json as string
+# print(type(resp.json()))
+# pprint(resp.json()) # prints the resp json as dictionary
+print(f"Resp status code -> {resp.status_code}") # used for debugging purpose after getting response
