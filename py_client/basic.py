2b9659755b56a084ea61d5d5c4c7ce97bf9cc684
Abhishek <abhibhovar@gmail.com>
2024-06-29 15:13:53 +0530

    Created a basic view

--- py_client/basic.py
+++ py_client/basic.py
@@ -7,10 +7,11 @@ REST API HTTP Request -> JSON Response
 
 
 # URL from which we can rach the API
-endpoint = "http://localhost:8000/"
+endpoint = "http://localhost:8000/api"
+print("Sending Response")
 resp = requests.get(endpoint) # sending the request
 # resp = requests.get(endpoint, json = {"query" : "Hello World"}) # sending request with some data
 # print(f"{type(resp.text)} : \n{resp.text}") # prints the resp json as string
-# print(type(resp.json()))
-pprint(resp.text) # prints the resp json as dictionary
+print(resp.json())
+# pprint(resp.text) # prints the resp json as dictionary
 # print(f"Resp status code -> {resp.status_code}") # used for debugging purpose after getting response
